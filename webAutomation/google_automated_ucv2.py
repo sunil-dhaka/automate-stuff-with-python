9d6c87fd32e2d2b99d6c972bed934b4c0e7535b0
elliot <dhaka.sonu.monu@gmail.com>
2022-04-06 22:04:18 +0530

    comment using pyautogui module after logging into the account

--- webAutomation/google_automated_ucv2.py
+++ webAutomation/google_automated_ucv2.py
@@ -1,10 +1,20 @@
 import undetected_chromedriver.v2 as uc
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
 from bs4 import BeautifulSoup as bs
 from helium import *
 from time import sleep
+import pyautogui
+
+# to be able to control window dor one second when things go south
+pyautogui.PAUSE=0.5
+pyautogui.FAILSAFE=True
+
+
 import login
+
+def google_login_undetecteed():
+    pass
 
 gmail=login.gmail
 password=login.password
@@ -32,42 +42,61 @@ driver = uc.Chrome(options=chrome_options)
 
 driver.delete_all_cookies()
 
-driver.get("https://accounts.google.com/signin/v2/identifier?hl=en&passive=true&continue=https%3A%2F%2Fwww.google.com%2F&ec=GAZAmgQ&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
-sleep(3)
-driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input").send_keys(gmail)
+driver.get("https://accounts.google.com/")
 sleep(2)
-driver.find_element_by_xpath("//*[@id='identifierNext']/div/button").send_keys(Keys.RETURN)
+driver.find_element(by='xpath',value="//*[@id='identifierId']").send_keys(gmail)
+sleep(0.5)
+driver.find_element(by='xpath',value="//*[@id='identifierNext']/div/button").send_keys(Keys.RETURN)
+sleep(2)
+driver.find_element(by='xpath',value='//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
+sleep(0.5)
+driver.find_element(by='xpath',value="//*[@id='passwordNext']/div/button").send_keys(Keys.RETURN)
+print(f'SUCCESS! Logged into {gmail} account.')
+sleep(2)
 
-sleep(2)
-driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
-sleep(2)
-driver.find_element_by_xpath("//*[@id='passwordNext']/div/button").send_keys(Keys.RETURN)
-sleep(3)
-driver.get('https://www.youtube.com/c/sbnclasses') # channel link as user input and number videos to be commented # or could give particular video link
-sleep(2)
-driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/tp-yt-app-header-layout/div/tp-yt-app-header/div[2]/tp-yt-app-toolbar/div/div/tp-yt-paper-tabs/div/div/tp-yt-paper-tab[2]').send_keys(Keys.RETURN)
-sleep(10)
+driver.get('https://www.youtube.com/c/sbnclasses/videos') # channel link as user input and number videos to be commented # or could give particular video link
+sleep(5)
+# driver.find_element(by='xpath',value='/html/body/ytd-app/div/ytd-page-manager/ytd-browse/div[3]/ytd-c4-tabbed-header-renderer/tp-yt-app-header-layout/div/tp-yt-app-header/div[2]/tp-yt-app-toolbar/div/div/tp-yt-paper-tabs/div/div/tp-yt-paper-tab[2]').send_keys(Keys.RETURN)
+# sleep(10)
 html=driver.page_source
 soup=bs(html,'html.parser')
 
 video_links=['https://www.youtube.com'+video.find(id='thumbnail').get('href') for video in soup.find('div',id='content').find_all('ytd-grid-video-renderer')]
+INTERVAL=0.1
+sleep(5)
+print('5 secs to switch to the main window.')
 
-for link in video_links[1:5]: # due to interent issue only 4 vids to test
-
+for i,link in enumerate(video_links): # due to interent issue only 4 vids to test
     driver.get(link)
     sleep(5)
     driver.execute_script("window.scrollTo(0,1000)")
     sleep(5)
+    commentable=True
     try:
-        comment_box=driver.find_element_by_id('simplebox-placeholder')
-        sleep(3)
-        comment_box.send_keys('This channel does have 🌟good quality🌟 video conetnt. Keep it up SBNCLASSES. 👍')
+        driver.find_element(by='id',value='simplebox-placeholder')
     except NoSuchElementException:
-        print('comments are off for this video.')
+        commentable=False
+
+    if commentable:
         sleep(2)
-        continue
-    sleep(2)
-    comment_submit=driver.find_element_by_css_selector("tp-yt-paper-button[aria-label='Comment']")
-    sleep(1)
-    comment_submit.send_keys(Keys.RETURN)
-    sleep(2)
+        """
+        gui automation
+        needs to keep this frame in front layer
+        """
+        # click into the comment box
+        pyautogui.click(194,316)
+        sleep(1)
+        comment_message='Disclaimer: automated bot comment. Easy to understand and free resources on the channel. GOOD!'
+        pyautogui.typewrite(comment_message,interval=INTERVAL)
+        sleep(1)
+        pyautogui.typewrite(['\t','\t','\n'],interval=INTERVAL)
+        sleep(1)
+        print(f'Commented for video {i+1}')
+    else:
+        print(f'Video {i+1} has comments turned off.')
+    # driver.find_element(by='id',value='simplebox-placeholder').send_keys('This channel does have 🌟good quality🌟 video conetnt. Keep it up SBNCLASSES. 👍')
+    # sleep(2)
+    # comment_submit=driver.find_element_by_css_selector("tp-yt-paper-button[aria-label='Comment']")
+    # sleep(1)
+    # comment_submit.send_keys(Keys.RETURN)
+    # sleep(2)
